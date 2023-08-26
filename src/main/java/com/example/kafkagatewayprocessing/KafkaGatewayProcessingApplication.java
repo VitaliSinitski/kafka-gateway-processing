f812b990b021fa648be2c57773a3b0b903c98353
Vitali Sinitski <vitali.sin777@gmail.com>
2023-08-26 22:42:30 +0300

    Base app

---
+++ src/main/java/com/example/kafkagatewayprocessing/KafkaGatewayProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkagatewayprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGatewayProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaGatewayProcessingApplication.class, args);
+	}
+
+}
